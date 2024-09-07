1f0b12656f92aa9916d2a526c65c642bf1f23048
limkhl <limkh.dev@gmail.com>
2024-09-08 00:31:25 +0900

    feat: next config에 images domains 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  images: {
+    domains: [
+      'cdn.litt.ly',
+      'cdn.imweb.me',
+      'www.sisters.or.kr',
+      'ywca.or.kr',
+      'women21.or.kr',
+      'womenfund.or.kr',
+      'www.nanumkorea.go.kr',
+      'raw.githubusercontent.com',
+      'cdn.huffingtonpost.kr',
+      'merryyear.org',
+      'encrypted-tbn0.gstatic.com',
+      'res.cloudinary.com',
+      'cdn.myportfolio.com',
+      'kumsn.org',
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
