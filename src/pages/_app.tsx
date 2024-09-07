e8deea4cd151cce504efbd32e024fa9f4e5a84a1
limkhl <limkh.dev@gmail.com>
2024-09-08 08:57:25 +0900

    feat: og 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,26 @@
+import Head from 'next/head'
 import { ParaglideJS } from '@inlang/paraglide-next/pages'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import * as m from '@/paraglide/messages'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="background-yellow">
-      <ParaglideJS>
-        <Component {...pageProps} />
-      </ParaglideJS>
-    </div>
+    <>
+      <Head>
+        <title>{m.meta_title()}</title>
+        <meta name="description" content={m.home_subtitle()} />
+        <meta property="og:title" content={m.meta_title()} />
+        <meta property="og:description" content={m.home_subtitle()} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/images/og-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div className="background-yellow">
+        <ParaglideJS>
+          <Component {...pageProps} />
+        </ParaglideJS>
+      </div>
+    </>
   )
 }
