100db9615a7d79458f89f7c3f52333f5d63349b5
Doori Kim <doori.alice.kim@gmail.com>
2024-09-08 00:18:54 +0900

    결과 페이지 추가

---
+++ src/pages/result/[id].tsx
@@ -0,0 +1,46 @@
+import { GetServerSidePropsContext } from "next";
+import Description from "@/components/Description";
+import data from "@/data/organzation.json";
+import SimilarOrgans from "@/components/similarOrgans";
+import Share from "@/components/Share";
+import Button from "@/components/Button";
+
+type ResultProps = {
+  type: string;
+};
+
+const ResultPage = ({ type }: ResultProps) => {
+  const result = data.find((item) => item.type === type);
+  if (!result) return null;
+  return (
+    <>
+      <Description
+        type={result.type}
+        name={result.name}
+        title={result.title}
+        url={result.mainUrl}
+        imageUrl={result.imageUrl}
+        description={result.description}
+        characteristics={result.characteristics}
+        matchingPoints={result.matchingPoints}
+      />
+      <SimilarOrgans organs={result.similarOrganizations} />
+      <Share />
+      <Button text="테스트 다시하기" href="/question" />
+      <Button text="후원 하러 가기" href={result.donationUrl} />
+      <Button text="목록으로" href="/list" />
+    </>
+  );
+};
+
+export default ResultPage;
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const query = ctx.query;
+  console.log(ctx, query);
+  return {
+    props: {
+      type: query.id,
+    },
+  };
+};
